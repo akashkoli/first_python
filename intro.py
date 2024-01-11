859295ff684540281c71e36a481fe1d69ff7d38c
Akash <akash.agits@gmail.com>
2024-01-11 07:59:15 +0530

    Python Variables

--- intro.py
+++ intro.py
@@ -4,3 +4,27 @@ print("Here we are working on Python Comments")
 """
 Here we have multi line comments for python
 """
+print("In Python we do not declare variables, the are declared automatically when assigned at first time")
+a = 4
+b=6
+c = a + b
+print("Addition of A + B = ",a+b)
+print("Addition using third variable C", c," - also we can concatinate")
+
+name = 'Akash Koli'
+print("Here I have a string in variable = ", name," let me know is this printed")
+print("Yes its printing properly")
+
+a = 15
+print("Here I am print a = ",a)
+A = "Koli"
+print("Here I am printing a agian for A", a)
+print("printing A to check case sensitive ", A)
+# Lets see here Vaiable Type Casting
+print('Variable Type Casting')
+x = str(10)
+y = int(5)
+z = float(8)
+print('This is converted to string', x)
+print("This is converted to int", y)
+print("This is converted to fload", z)
